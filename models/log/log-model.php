d9c9c36b238d860085ea6d18a66e87d76a79b482
Fabiano Hatori <sistemaeficaz@sistema.eficazsystem.com.br>
2017-04-03 17:39:44 -0300

    03042017 - iniciando implementacao de log de logins

---
+++ models/log/log-model.php
@@ -0,0 +1,24 @@
+<?php
+
+class LogModel extends MainModel
+{
+
+    public function __construct ($db = false, $controller = null)
+    {
+        /* carrega a conexao */
+        $this->db = $db;
+
+        /* carrega o controller */
+        $this->controller = $controller;
+
+        /* carrega os parametros */
+        $this->parametros = $this->controller->parametros;
+    }
+
+
+
+    
+
+}
+
+?>
