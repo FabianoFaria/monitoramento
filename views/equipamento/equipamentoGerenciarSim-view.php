87da067b284500a38e8be3110b0e6fbce6f25120
Fabiano Hatori <sistemaeficaz@sistema.eficazsystem.com.br>
2017-04-13 14:11:25 -0300

    13042017 - teste de deploy

--- views/equipamento/equipamentoGerenciarSim-view.php
+++ views/equipamento/equipamentoGerenciarSim-view.php
@@ -118,7 +118,7 @@
 <!-- MODAL PARA CADASTRO DE CHIP SIM -->
 <div id="modalCadastroChip" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
     <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
+        <div class="modal-content"> 
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="myModalLabelCad">Cadastro de novo chip SIM</h4>
